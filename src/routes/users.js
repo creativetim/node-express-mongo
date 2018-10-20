9500e99daf131ee4e6ab01cdd0efd972958d7c15
Tim Yager <tim.yager@gmail.com>
2018-10-20 18:33:09 -0500

    Register form has basic validation and error flashes.

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,5 +10,38 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
+router.post('/register', (req, res) => {
+    const errors = [];
+    const {
+        name,
+        email,
+        password,
+        password2,
+    } = req.body;
+
+    if (password !== password2) {
+        errors.push({ message: 'Passwords do not match' });
+    }
+
+    if (password.length < 4) {
+        errors.push({ message: 'Password must be at least 4 characters' });
+    }
+
+    if (errors.length) {
+        res.render('users/register', {
+            errors,
+            messages: {
+                errors,
+            },
+            name,
+            email,
+            password,
+            password2,
+        });
+    } else {
+        res.send('register me!');
+    }
+});
+
 
 module.exports = router;
