fd444380619ccf6b0a2c714ce03bd052b78d51ca
Tim Yager <tim.yager@gmail.com>
2018-10-20 17:19:34 -0500

    Got flash messaging working; diverged a bit from the tutorial because I am using Express 4.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('express-flash');
 
 const app = express();
 const port = 5000;
@@ -9,7 +13,7 @@ const port = 5000;
 
 // MongoDB
 mongoose
-    .connect('mongodb://localhost/vidjot-dev')
+    .connect('mongodb://localhost/vidjot-dev', { useNewUrlParser: true })
     .then(() => console.log('MongoDB connected.'))
     .catch(err => console.error(err));
 
@@ -20,14 +24,25 @@ const Idea = mongoose.model('ideas');
 // Middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main',
+    layoutsDir: './src/views/layouts',
+    partialsDir: './src/views/partials',
 }));
 app.set('view engine', 'handlebars');
+app.set('views', './src/views');
 
 app.use(bodyParser.urlencoded({
     extended: false,
 }));
 app.use(bodyParser.json());
-
+app.use(methodOverride('_method'));
+app.use(cookieParser('rouge-uh-douge'));
+app.use(session({
+    cookie: { maxAge: 60000 },
+    secret: 'rouge-uh-douge',
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(flash());
 
 // Routes
 app.get('/', (req, res) => {
@@ -37,6 +52,7 @@ app.get('/', (req, res) => {
         title,
     });
 });
+
 app.get('/about',(req, res) => {
     res.render('about');
 });
@@ -48,6 +64,7 @@ app.get('/ideas', (req, res) => {
             ideas,
         }));
 });
+
 app.post('/ideas', (req, res) => {
     let errors = [];
 
@@ -73,11 +90,46 @@ app.post('/ideas', (req, res) => {
 
         new Idea(newUser)
             .save()
-            .then(idea => res.redirect('/ideas'));
+            .then(idea => {
+                req.flash('success', 'Video Idea was added');
+                res.redirect('/ideas');
+            });
     }
 });
+
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({ _id: req.params.id })
+        .then(idea => {
+            idea.title = req.body.title;
+            idea.details = req.body.details;
+
+            idea.save()
+                .then(idea => {
+                    req.flash('success', 'Video Idea was edited');
+                    res.redirect('/ideas');
+                });
+        })
+});
+
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({ _id: req.params.id })
+        .then(() => {
+            req.flash('error', 'Video Idea was deleted');
+            res.redirect('/ideas');
+        });
+});
+
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+});
+
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({ _id: req.params.id })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea,
+            });
+        });
 });
 
 
